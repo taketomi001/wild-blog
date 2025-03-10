5ce39c554d4d2b63b68f261440831b14d04ba0cb
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-10 20:05:43 +0100

    first push

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Bienvenue sur le Wild Blog de CE !-blog';
+}
