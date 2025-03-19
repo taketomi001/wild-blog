ae1168290ee59be00fb2b4f923b4ddb8d2393a93
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-03-19 11:19:02 +0100

    update router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ArticlePageComponent } from './pages/article-page/article-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 // import { ArticlePageComponent } from './components/article-list';
 // import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Accueil
   { path: 'article/:id', component: ArticlePageComponent }, // Détails d'un article
-//   { path: '**', component: NotFoundPageComponent } // Page 404
+  { path: '**', component: NotFoundPageComponent } // Page 404
 ];
 
 @NgModule({
